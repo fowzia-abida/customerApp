d2b3be87469edccb8515f3bf79851cf02fcf8bfb
Fowzia Abida <fowzia.abida21@gmail.com>
2022-12-27 03:30:24 +0100

    Creating UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Customer} from "./app.model";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'customerApp';
+  CustomerModel : Customer = new Customer(); //customer component is creating a object CustomerModel which we need to bind in HTML file with ngModel;
+  // it is for the single record talks with the text boxes
+   DisplayBelowTable : Array<Customer> = new Array<Customer>()//it is for collection which will talk with table
+  Add(){  // when the add button is clicked
+     this.DisplayBelowTable.push(this.CustomerModel)
+    this.CustomerModel = new Customer();
+ //clear UI and attach new customer obj
+
+  }
 }
